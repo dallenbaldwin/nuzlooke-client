5385efe76ca95fc45496436d037cd084dbbbdb01
Dallen Baldwin <dallen.baldwin@gmail.com>
2021-03-03 23:01:27 -0700

    got started with the code

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,58 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+   <v-app>
+      <v-app-bar elevation="1" bottom app>
+         <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+         <v-spacer></v-spacer>
+         <v-btn icon>
+            <v-icon>mdi-filter</v-icon>
+         </v-btn>
+      </v-app-bar>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+      <v-navigation-drawer v-model="drawer" absolute bottom temporary>
+         <v-list nav>
+            <v-list-item-group
+               v-model="group"
+               active-class="deep-purple--text text--accent-4"
+            >
+               <v-list-item>
+                  <v-list-item-title>Foo</v-list-item-title>
+               </v-list-item>
 
-      <v-spacer></v-spacer>
+               <v-list-item>
+                  <v-list-item-title>Bar</v-list-item-title>
+               </v-list-item>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
+               <v-list-item>
+                  <v-list-item-title>Fizz</v-list-item-title>
+               </v-list-item>
 
-    <v-main>
-      <HelloWorld/>
-    </v-main>
-  </v-app>
+               <v-list-item>
+                  <v-list-item-title>Buzz</v-list-item-title>
+               </v-list-item>
+            </v-list-item-group>
+         </v-list>
+      </v-navigation-drawer>
+
+      <v-main>
+         <v-container>
+            <router-view></router-view>
+         </v-container>
+      </v-main>
+   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
-export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
-};
+   export default {
+      name: 'App',
+      components: {},
+      data: () => ({
+         drawer: false,
+         group: null,
+      }),
+      watch: {
+         group() {
+            this.drawer = false;
+         },
+      },
+   };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import './registerServiceWorker';
+import router from './router/router.index';
+import store from './store/store.index';
 import vuetify from './plugins/vuetify';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  vuetify,
-  render: function (h) { return h(App) }
-}).$mount('#app')
+   router,
+   store,
+   vuetify,
+   render: function(h) {
+      return h(App);
+   },
+}).$mount('#app');
