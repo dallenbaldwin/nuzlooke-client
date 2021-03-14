775d5ce6e157bba40d3bc4f9b93fce07611cc7b3
Dallen Baldwin <dallen.baldwin@gmail.com>
2021-03-14 16:52:49 -0600

    added some stuff for setting the user

--- src/mixins/util.js
+++ src/mixins/util.js
@@ -1,33 +1,57 @@
 import { isMobile } from 'mobile-device-detect';
+import { getUserById } from '../services/user.js';
+import store from '../store/store.js';
+import router from '../router/router.js';
 
 export default {
    methods: {
       navigate(endpoint) {
-         this.$router.push(endpoint);
+         console.log(`navigating to ${endpoint}`);
+         router.push(endpoint);
       },
       getRouteInfo() {
          return {
-            name: this.$route.name,
-            path: this.$route.path,
-            params: this.$route.params,
+            name: router.currentRoute.name,
+            path: router.currentRoute.path,
+            params: router.currentRoute.params,
          };
       },
       logout() {
-         this.$store.commit('exitGame');
-         this.$store.commit('logout');
+         store.commit('exitGame');
+         store.commit('logout');
          this.navigate({ name: 'home' });
       },
       login() {
-         // FIXME: this is a dummy method to test the store
-         this.$store.commit('login', { username: 'dallen.baldwin' });
-         this.navigate({ name: 'games' });
+         // FIXME: this will need to be reworked when we get to authorization
+         getUserById('9d0f3fca-f516-479d-8d15-7260c33a55f8')
+            .then(res => {
+               let user = res.data.response.data;
+               store.commit('login', {
+                  id: user.id,
+                  username: user.username || user.email,
+                  games: user.games,
+               });
+               this.navigate({ name: 'games' });
+            })
+            .catch(err => console.log);
       },
       exitGame() {
-         this.$store.commit('exitGame');
+         store.commit('exitGame');
          this.navigate({ name: 'games' });
       },
       mobile() {
          return isMobile;
+      },
+      arrayify(...values) {
+         const a = [];
+         values.forEach(value => a.push(value));
+         return a.flatMap(i => i); // force it to accept an array of values or a list of values
+      },
+      deClassify(classObject) {
+         return JSON.parse(JSON.stringify(classObject));
+      },
+      isUndefined(value) {
+         return value === undefined || value === null;
       },
    },
 };
