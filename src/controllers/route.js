e8ab811605e6e7211ca22be6c04ad11209109632
Dallen Baldwin <dallen.baldwin@gmail.com>
2021-03-24 22:05:55 -0600

    consolidate controllers!

--- src/controllers/route.js
+++ src/controllers/route.js
@@ -1,10 +1,55 @@
 import store from '../store/store.js';
+import RuleCodes from '../constants/RuleCodes.js';
+import EncounterResultConst from '../constants/EncounterResultConst.js';
+import * as util from '../util/util.js';
+import * as rulesController from '../controllers/rules.js';
 
 export const getEncounterById = id => {
-   console.log(id);
    return store.state.game.encounters.find(e => id === e.id);
 };
 
 export const updateEncounterById = encounter => {
    store.commit('updateEncounterById', { encounter: encounter });
+};
+
+export const isAvailable = constant => constant === EncounterResultConst.AVAILABLE;
+
+export const isCaught = constant => constant === EncounterResultConst.CAUGHT;
+
+export const isFledFainted = constant =>
+   [EncounterResultConst.FAINTED, EncounterResultConst.FLED].includes(constant);
+
+export const getEncounterErrors = results => {
+   const errors = [];
+   const species = !util.isUndefined(results.species)
+      ? results.species.text
+      : results.species;
+   if (isCaught(results.constant)) {
+      if (
+         rulesController.isActive(RuleCodes.USE_NICKNAMES.code) &&
+         util.isUndefined(results.nickname)
+      ) {
+         errors.push('Captured Pokemon need a nickname!');
+      }
+      if (util.isUndefined(species)) {
+         errors.push(`You must select a Pokemon!`);
+      }
+   } else if (isAvailable(results.constant)) {
+      if (!util.isUndefined(species)) {
+         errors.push(
+            `Something went wrong. Must be Fainted, Fled, or Caught, to have a Pokemon.`
+         );
+      }
+      if (!util.isUndefined(results.nickname)) {
+         errors.push(`Something went wrong. Must be Caught to have a nickname.`);
+      }
+   } else if (isFledFainted(results.constant)) {
+      if (!util.isUndefined(results.nickname)) {
+         errors.push(`Something went wrong. Must be Caught to have a nickname.`);
+      }
+      if (util.isUndefined(species)) {
+         errors.push(`You must select a Pokemon!`);
+      }
+   }
+   return errors;
 };
