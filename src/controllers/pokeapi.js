03f763424aee02bf827b74a466e833bd82aaf19b
Dallen Baldwin <dallen.baldwin@gmail.com>
2021-03-24 22:07:21 -0600

    wrap results better

--- src/controllers/pokeapi.js
+++ src/controllers/pokeapi.js
@@ -1,18 +1,23 @@
 import * as pokeapiServices from '../services/pokeapi.js';
 import UserPokemon from '../models/UserPokemon.js';
+import { errorCatch } from '../util/util.js';
 
 export async function buildUserPokemon(result) {
-   const pokeData = await pokeapiServices.getPokemonBySpecies(result.species.text);
-   const speciesData = await pokeapiServices.get(pokeData.species.url);
-   const evoData = await pokeapiServices.get(speciesData.evolution_chain.url);
-   return UserPokemon.builder()
-      .withSpecies(normalizeKabob(pokeData.species.name))
-      .withNickname(result.nickname)
-      .withSpritUrl(pokeData.sprites.front_default)
-      .withIconUrl(pokeData.sprites.versions['generation-vii'].icons.front_default)
-      .withTypes(pokeData.types.map(t => normalizeKabob(t.type.name)))
-      .withEvolvesTo(evoData.chain.evolves_to.map(e => normalizeKabob(e.species.name)))
-      .build();
+   try {
+      const pokeData = await pokeapiServices.getPokemonBySpecies(result.species.text);
+      const speciesData = await pokeapiServices.get(pokeData.species.url);
+      const evoData = await pokeapiServices.get(speciesData.evolution_chain.url);
+      return UserPokemon.builder()
+         .withSpecies(normalizeKabob(pokeData.species.name))
+         .withNickname(result.nickname)
+         .withSpritUrl(pokeData.sprites.front_default)
+         .withIconUrl(pokeData.sprites.versions['generation-vii'].icons.front_default)
+         .withTypes(pokeData.types.map(t => normalizeKabob(t.type.name)))
+         .withEvolvesTo(evoData.chain.evolves_to.map(e => normalizeKabob(e.species.name)))
+         .build();
+   } catch (err) {
+      alert(errorCatch(err));
+   }
 }
 
 const normalizeKabob = (string = '') =>
