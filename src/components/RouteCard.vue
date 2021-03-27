99e5d864072d49f8179b4a22e7cc31f64be63ae4
Dallen Baldwin <dallen.baldwin@gmail.com>
2021-03-27 01:46:58 -0600

    saw how great transitions are

--- src/components/RouteCard.vue
+++ src/components/RouteCard.vue
@@ -1,52 +1,54 @@
 <template>
-   <v-card
-      min-width="300"
-      max-width="300"
-      elevation="1"
-      outlined
-      class="align-self-start mt-3 mx-3"
-   >
-      <div v-if="isAvailable">
-         <v-card-title>{{ encounter.label }}</v-card-title>
-         <v-card-subtitle>Available Pokemon</v-card-subtitle>
-         <v-card-text v-if="isAvailable" class="d-inline-flex justify-end">
-            <c-poke-sprite
-               class="mx-1"
-               v-for="pokemon of encounter.pokemons"
-               :key="pokemon.species"
-               :src="pokemon.icon_url"
-               size="small"
-            ></c-poke-sprite>
-         </v-card-text>
-      </div>
-      <div v-if="!isAvailable" class="d-inline-flex justify-start">
-         <c-poke-sprite
-            class="align-self-start mx-3 mt-3"
-            :src="encounter.result.sprite_url"
-            :outline-color="outLineColor"
-         ></c-poke-sprite>
-         <span>
+   <v-fade-transition>
+      <v-card
+         min-width="300"
+         max-width="300"
+         elevation="1"
+         outlined
+         class="align-self-start mt-3 mx-3"
+      >
+         <div v-if="isAvailable">
             <v-card-title>{{ encounter.label }}</v-card-title>
-            <v-card-subtitle
-               >{{ encounter.result.constant
-               }}{{
-                  encounter.result.nickname ? `: ${encounter.result.nickname}` : ''
-               }}</v-card-subtitle
-            >
-         </span>
-      </div>
-      <v-card-actions class="pt-0 pb-0 mb-2">
-         <v-spacer></v-spacer>
-         <v-btn text @click="emitNewEncounter" v-if="isAvailable">
-            <v-icon color="green" dark>{{ Icons.CONTROLS.EXCLAIM }}</v-icon>
-            Encounter
-         </v-btn>
-         <v-btn text @click="emitResetEncounter" v-if="!isAvailable">
-            <v-icon color="orange" dark>{{ Icons.CONTROLS.EXCLAIM }}</v-icon>
-            Reset
-         </v-btn>
-      </v-card-actions>
-   </v-card>
+            <v-card-subtitle>Available Pokemon</v-card-subtitle>
+            <v-card-text v-if="isAvailable" class="d-inline-flex justify-end">
+               <c-poke-sprite
+                  class="mx-1"
+                  v-for="pokemon of encounter.pokemons"
+                  :key="pokemon.species"
+                  :src="pokemon.icon_url"
+                  size="small"
+               ></c-poke-sprite>
+            </v-card-text>
+         </div>
+         <div v-if="!isAvailable" class="d-inline-flex justify-start">
+            <c-poke-sprite
+               class="align-self-start mx-3 mt-3"
+               :src="encounter.result.sprite_url"
+               :outline-color="outLineColor"
+            ></c-poke-sprite>
+            <span>
+               <v-card-title>{{ encounter.label }}</v-card-title>
+               <v-card-subtitle
+                  >{{ encounter.result.constant
+                  }}{{
+                     encounter.result.nickname ? `: ${encounter.result.nickname}` : ''
+                  }}</v-card-subtitle
+               >
+            </span>
+         </div>
+         <v-card-actions class="pt-0 pb-0 mb-2">
+            <v-spacer></v-spacer>
+            <v-btn text @click="emitNewEncounter" v-if="isAvailable">
+               <v-icon color="green" dark>{{ Icons.CONTROLS.EXCLAIM }}</v-icon>
+               Encounter
+            </v-btn>
+            <v-btn text @click="emitResetEncounter" v-if="!isAvailable">
+               <v-icon color="orange" dark>{{ Icons.CONTROLS.EXCLAIM }}</v-icon>
+               Reset
+            </v-btn>
+         </v-card-actions>
+      </v-card>
+   </v-fade-transition>
 </template>
 
 <script>
