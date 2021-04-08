61a39b0c36304a68e476da0efd6dea413a4de489
Dallen Baldwin <dallen.baldwin@gmail.com>
2021-04-08 00:23:50 -0600

    added text area component

---
+++ src/components/form-controls/TextArea.vue
@@ -0,0 +1,27 @@
+<template>
+   <v-textarea
+      :clear-icon="Icons.CONTROLS.CLOSE"
+      clearable
+      outlined
+      :value="value"
+      @input="$emit('input', $event)"
+      :label="label"
+      :disabled="disabled"
+      auto-grow
+      rows="1"
+   >
+   </v-textarea>
+</template>
+
+<script>
+export default {
+   name: 'TextField',
+   props: {
+      label: { required: true },
+      value: { required: false },
+      disabled: { required: false, default: false },
+   },
+};
+</script>
+
+<style></style>
