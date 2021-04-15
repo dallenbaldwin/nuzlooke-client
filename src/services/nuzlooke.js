6cba93413b9e474c4dcc312556b20422722c9a47
Dallen Baldwin <dallen.baldwin@gmail.com>
2021-04-15 11:32:43 -0600

    updating api baseURL for nuzlooke server

--- src/services/nuzlooke.js
+++ src/services/nuzlooke.js
@@ -5,7 +5,7 @@ let port = '3000';
 let baseUrl = `http://localhost:${port}/api`;
 
 if (process.env.NODE_ENV === 'production') {
-   baseUrl = `http://nuzlooke-env.eba-pqxywj6d.us-east-2.elasticbeanstalk.com/api`;
+   baseUrl = `http://nuzlooke.us-east-2.elasticbeanstalk.com/api`;
 }
 
 export default () => {
