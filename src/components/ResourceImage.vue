f845cff0581ce021f7f8bf361cb6845311437684
Dallen Baldwin <dallen.baldwin@gmail.com>
2021-04-29 23:14:25 -0600

    resource image component

---
+++ src/components/ResourceImage.vue
@@ -0,0 +1,31 @@
+<template>
+   <div>
+      <v-img
+         contain
+         :alt="alt"
+         :src="src"
+         aspect-ratio="3/2"
+         :max-width="maxWidth"
+         :lazy-src="Icons.MISC.NUZLOOKE"
+         position="center left"
+      ></v-img>
+   </div>
+</template>
+
+<script>
+export default {
+   name: 'ResourceImage',
+   props: {
+      alt: { required: true },
+      src: { required: true },
+      'max-width': { required: true },
+   },
+   data() {
+      return {};
+   },
+   computed: {},
+   methods: {},
+};
+</script>
+
+<style></style>
