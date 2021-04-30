755485537ddc4c24770592a87f0a687971901afa
Dallen Baldwin <dallen.baldwin@gmail.com>
2021-04-29 23:12:20 -0600

    pwa stuff

--- vue.config.cjs
+++ vue.config.cjs
@@ -1,3 +1,8 @@
 module.exports = {
    transpileDependencies: ['vuetify'],
+   pwa: {
+      msTileColor: '#fff',
+      appleMobileWebAppCapable: 'yes',
+      appleMobileWebAppStatusBarStyle: 'black',
+   },
 };
